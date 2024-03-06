900daf3a48a23d24873ec38e42631ecfbcdabbe8
Bogdan Grabinskij <admin@example.com>
2024-03-06 11:18:34 +0100

    Add cookie consent banner

--- app/js/main.js
+++ app/js/main.js
@@ -114,3 +114,135 @@ $(function(){
 });
 
 
+
+
+//Cookie banner language changer
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const cookieGermanBtn = document.getElementById('cookieGermanBtn');
+    const cookieEnglishBtn = document.getElementById('cookieEnglishBtn');
+    const cookieEnglish = document.getElementById('cookieEnglish');
+    const cookieGerman = document.getElementById('cookieGerman');
+  
+  
+    cookieGermanBtn.addEventListener('click', () => {
+        cookieEnglish.style.display = 'block';
+        cookieGerman.style.display = 'none';
+    });
+  
+    cookieEnglishBtn.addEventListener('click', () => {
+        cookieGerman.style.display = 'block';
+        cookieEnglish.style.display = 'none';
+    });
+  
+  });
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const cookieStorage = {
+      getItem: (key) => {
+        const cookies = document.cookie
+          .split(';')
+          .map(cookie => cookie.split('='))
+          .reduce((acc, [key, value]) => ({
+            ...acc,
+            [key.trim()]: value
+          }), {});
+  
+        return cookies[key];
+      },
+      setItem: (key, value) => {
+        document.cookie = `${key}=${value};expires=Sun, 17 Jul 3024 08:01:01 GMT`;
+      }
+    };
+  
+  
+  
+  
+    const storageType = cookieStorage;
+    const consentPropertyType = 'site_consent';
+  
+    let hasConsented = () => storageType.getItem(consentPropertyType) === "true" ? true : false;
+    let toggleStorage = (prop) => storageType.setItem(consentPropertyType, prop);
+    let isPopupVisible = false;
+  
+  
+    const cookiePopup = document.getElementById('cookiePopup');
+    const overlay = document.getElementById('overlay');
+    const acceptCookiesBtns = document.querySelectorAll('.acceptCookiesBtn');
+    const declineCookiesBtns = document.querySelectorAll('.declineCookiesBtn');
+    const changeConsentBtn = document.querySelector('.change-consent-banner');
+    const googleMapsIframe = document.querySelector('.map');
+  
+  
+    function showGoogleMapsIframe() {
+      googleMapsIframe.style.display = 'block';
+    }
+
+    function removeGoogleMapsIframe() {
+      googleMapsIframe.style.display = 'none';
+    }
+  
+  
+    function showChangeConsent() {
+      changeConsentBtn.style.display = 'block';
+    }
+  
+    function showCookiePopup() {
+      overlay.style.display = 'block';
+      cookiePopup.style.display = 'block';
+    }
+  
+    function hideCookiePopup() {
+      overlay.style.display = 'none';
+      cookiePopup.style.display = 'none';
+    }
+  
+  
+  
+   
+  
+    if (hasConsented()) {
+      showGoogleMapsIframe();
+      changeConsentBtn.style.display = 'block';
+    }
+    else {
+      showCookiePopup();
+      removeGoogleMapsIframe();
+    }
+    if (storageType.getItem(consentPropertyType) === "false") {
+      hideCookiePopup();
+      removeGoogleMapsIframe();
+      showChangeConsent();
+    }
+  
+    acceptCookiesBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      toggleStorage(true);
+      hideCookiePopup();
+      window.location.reload();
+  
+    });
+  });
+  
+  
+    declineCookiesBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        toggleStorage(false);
+        hideCookiePopup();
+        showChangeConsent();
+        window.location.reload();
+      });
+    });
+  
+  
+    changeConsentBtn.addEventListener('click', () => {
+      if (isPopupVisible) {
+        hideCookiePopup();
+      } else {
+        showCookiePopup();
+      }
+      isPopupVisible = !isPopupVisible;
+    });
+  });
